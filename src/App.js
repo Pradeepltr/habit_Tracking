09ca5bb2652b74a1aa30cb6d5e45a1e9046490a8
Pradeep Kumar <pk6361439@gmail.com>
2022-03-14 23:40:44 +0530

    homepage rendering issue solve

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 import Home from "./Components/Home"
 import Status from './Components/Status';
 import Update from './Components/Update';
-import {BrowserRouter as Router,Switch,Routes,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Switch,Routes,Route,Redirect} from 'react-router-dom'
+
 
 function App() {
 
@@ -17,9 +18,10 @@ function App() {
       <>
       
       <Routes>
-        <Route exact path='/' element={<Home />}/>
-        <Route exact path='/status' element={<Status />}/>
-        <Route exact path='/update' element={<Update />}/>
+        <Route  path='/home' element={<Home />}/>
+        <Route  path='/status' element={<Status />}/>
+        <Route  path='/update' element={<Update />}/>
+        
       </Routes>
       </>
     </Router>
